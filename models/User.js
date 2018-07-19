73f043e458fe32f491e96586b7e2cd6af121f530
Christopher Johnson <cjhawk7@hotmail.com>
2018-07-18 22:12:00 -0700

    changes

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
   avatar: { type: String },
   date: { type: Date, default: Date.now }
 });
--- routes/api/users.js
+++ routes/api/users.js
@@ -19,7 +19,20 @@ router.post("/register", (req, res) => {
 
       const newUser = new User({
         name: req.body.name,
-        email: req.body.email
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
