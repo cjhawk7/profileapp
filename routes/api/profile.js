deee4ada83bff4195a32b68e1a762f72ace13ecc
Christopher Johnson <cjhawk7@hotmail.com>
2018-07-19 22:29:15 -0700

    post endpoing cleaned up fields

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,5 +27,57 @@ router.get(
       .catch(err => res.status(404).json(err));
   }
 );
+//create user profile
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    const standardields = [
+      "handle",
+      "company",
+      "location",
+      "bio",
+      "status",
+      "githubUsername"
+    ];
+    socialFields = ["youtube", "twitter", "facebook", "linkedin", "instagram"];
+    standardFields.forEach(field => {
+      if (req.body[field]) profileFields[field] = req.body[field];
+    });
+
+    profileFields.social = {};
+
+    socialFields.forEach(field => {
+      if (req.body[field]) profileFields.social[field] = req.body[field];
+    });
+    // Skills - Spilt into array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        //check if handle exists
+        Profile.findOne({ handle: profileField.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
+          //save profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+  }
+);
 
 module.exports = router;
