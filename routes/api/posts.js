742434cdfbeb42e5760b6961f98b16e95be3c2e5
Christopher Johnson <cjhawk7@hotmail.com>
2018-07-21 16:31:39 -0700

    delete by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -68,4 +68,87 @@ router.delete(
   }
 );
 
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          }
+
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
+        })
+
+        .catch(err =>
+          res.status(404).json({ postnotfound: "Post not found." })
+        );
+    });
+  }
+);
+
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.body.user
+        };
+
+        post.comments.unshift(newComment);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+//remove comment
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.comments.find(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res.status(404).json({ nocomment: "Comment does not exist" });
+        }
+
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        if (req.user.id !== post.comments[removeIndex].user.toString()) {
+          return res.status(401).json({ notauthorized: "User not authorized" });
+        }
+
+        post.comments.splice(removeIndex, 1);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
