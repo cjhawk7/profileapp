a19ddc8d56ca6f410ed4742b31ad95a61dd57477
Christopher Johnson <cjhawk7@hotmail.com>
2018-07-21 19:23:12 -0700

    nav/footer component

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return (
+    //   using emmett for styling
+    <footer className="bg-dark text-white.mt-5.p-4 text-center">
+      <a className="nav-link" href="login.html">
+        stuff here
+      </a>
+    </footer>
+  );
+};
