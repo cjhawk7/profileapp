e3baa61158bf726eb14a7a0a1d7b5119e8f10ad6
Christopher Johnson <cjhawk7@hotmail.com>
2018-07-24 17:37:36 -0700

    set auth token in source

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,9 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import qs from "qs";
+import setAuthToken from "../utils/setAuthToken";
+//npm i jwt-decode
+import jwt_decode from "jwt-decode";
 
 export const registerUser = (userData, history) => dispatch => {
   axios
@@ -12,4 +15,31 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
